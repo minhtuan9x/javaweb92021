8b559cd6d7ebcc4835b808b3784250073108b9e0
Minhtuan9x <0mtmtmt0@gmail.com>
2021-10-28 11:32:29 +0700

    Up ngày 28/10

--- src/main/java/com/dominhtuan/util/ParseValidateUtil.java
+++ src/main/java/com/dominhtuan/util/ParseValidateUtil.java
@@ -3,12 +3,12 @@ package com.dominhtuan.util;
 import com.dominhtuan.constant.SystemConstant;
 
 public class ParseValidateUtil {
-    public static Object parseValidate(Object str) {
-        if (str == null || str.equals(SystemConstant.EMPTY_STRING))
+    public static Object parseValidate(Object obj) {
+        if (obj == null || obj.toString().equals(SystemConstant.EMPTY_STRING))
             return null;
-        if(str.toString().matches("[0-9]{1,}")){
-            return Integer.parseInt(str.toString());
+        if(obj.toString().matches("[0-9]{1,}")){
+            return Integer.parseInt(obj.toString());
         }
-        return str.toString();
+        return obj.toString();
     }
 }
